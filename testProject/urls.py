06f86f2e503f6a27cbed8a36b18533a16e5538e0
nikhil8182 <nikhildeepak8182@gmail.com>
2022-01-27 21:50:51 +0530

    first commit

---
+++ testProject/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path
+
+
+def index(request):
+    return HttpResponse('<h1>Hello this is a test project</h1>')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index)
+]
