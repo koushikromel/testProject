44b2e6e8197985104567b112d0e1d6306205b375
Koushik Romel <koushikromel@gmail.com>
2022-01-27 22:03:37 +0530

    heroku

--- testProject/urls.py
+++ testProject/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 
 
 def index(request):
-    return HttpResponse('<h1>Hello this is a test project</h1>')
+    return HttpResponse('<h1>Hello this is a test project for heroku</h1>')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
